dff694a3365c2761f945b65124eebd46c3e6e23d
Kyle <kbrooks353@gmail.com>
2020-10-28 21:34:08 -0700

    rewrite

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const port = 8000;
 
 const requestListener = function(req, res){
     
-    if(req.headers.host && req.headers.host.startsWith('www')){
+    if(req.headers.host && req.headers.host.startsWith('www.')){
         console.log("hi");
         console.log(req);
 
-        res.writeHead(301, {Location: req.protocol + host + req.path});
+        res.writeHead(301, {Location: req.protocol + req.headers.host.slice(4) + req.path});
         return;
     }
     fs.readFile(__dirname + "/index.html")
