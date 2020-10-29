b9d1d5c42b491f5a9639b8bbfab322e363bc6831
Kyle <kbrooks353@gmail.com>
2020-10-28 21:24:16 -0700

    rewrite

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const requestListener = function(req, res){
     
     if(req.headers.host && req.headers.host.startsWith('www')){
         console.log("hi");
-        console.log(req.url);
+        console.log(req);
 
         res.writeHead(301, {Location: req.protocol + host + req.path});
         return;
