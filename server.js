574441da64ea30c9790a435759953e05d948384f
Kyle <kbrooks353@gmail.com>
2020-10-28 21:06:37 -0700

    rewrite url

--- server.js
+++ server.js
@@ -30,4 +30,5 @@ const requestListener = function(req, res){
 const server = http.createServer(requestListener);
 server.listen(port, host, () => {
     console.log('Server is running on http://')
+    console.log(url);
 })
