f7f23402633f4b61249c8c5d7efac1713c2fa432
Kyle <kbrooks353@gmail.com>
2020-10-30 14:12:42 -0700

    figuring out cannot get /

--- client/server/index.js
+++ client/server/index.js
@@ -8,7 +8,7 @@ app.use(express.static("../public"));
 
 // routing
 if(process.env.NODE_ENV === 'production') {
-    app.get('/*', function (req, res) {
+    app.get(function (req, res) {
          res.sendFile(path.join(__dirname, 'build', 'index.html'));
     });
   }
