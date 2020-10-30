88ecf8c9b1632071928efd3c199f6f926d7020f1
Kyle <kbrooks353@gmail.com>
2020-10-30 13:08:39 -0700

    changed to create react app + express server

---
+++ client/server/index.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const express = require("express");
+const app = express(); // create express app
+
+// add middleware
+app.use(express.static(path.join(__dirname, "..", "build")));
+app.use(express.static("../public"));
+
+// routing
+if(process.env.NODE_ENV === 'production') {
+    app.get('/*', function (req, res) {
+         res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    });
+  }
+
+// start express server on port 8000
+app.listen(8000, () => {
+  console.log("server started on port 8000");
+});
